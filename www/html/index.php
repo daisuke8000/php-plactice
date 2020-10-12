7a1ca091dab484de74707b57076ef58705c88d27
daisuke8000 <daisuke12108000@gmail.com>
2020-10-13 02:58:44 +0900

    php variables and constants

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,16 @@
 <?php
-phpinfo();
+
+$int = 123;
+$str = "test";
+$var = $str . $int;
+
+echo "変数の結合 : " . $var;
+echo "<br />";
+
+var_dump($var);
+echo "<br />";
+
+const MAX = 10;
+echo "定数 : " . MAX;
+
 ?>
